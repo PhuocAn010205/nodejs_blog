91ab0559b58013435f2069218ee152fea8d7957b
PhuocAn010205 <huynh.an.10a1@gmail.com>
2025-04-25 23:46:02 +0700

    xong

--- tu_2.js
+++ tu_2.js
@@ -1,6 +1,21 @@
-function checkDataType(value){
-   return  typeof value;
-}
-console.log(checkDataType(42)); // number
-console.log(checkDataType("Hello world")); // string
-console.log(checkDataType({name: "John"})); // object
+// kiểm tra 2 value có cần kiểu dữ liệu không hay;
+function checkType(value1, value2) {
+  if (typeof value1 === typeof value2) {
+    return "cung kiểu dữ liệu";
+  }
+  return "khác kiểu dữ liệu";
+
+};
+//muốn kiểm tra dữ liệu thì không nên dùng if ở sau return vì lúc đó code chỉ chạy tới 
+// return thì đã ra kết quả rồi, không chạy tiếp được nữa
+// ví dụ sau
+// function checkType(value1, value2) {
+  // if (typeof value1 === typeof value2) {
+  //   return "cung kiểu dữ liệu";
+  // }
+  // return "khác kiểu dữ liệu";}
+
+
+
+console.log(checkType(1, 2)); // true
+console.log(checkType(1, '2')); // false
